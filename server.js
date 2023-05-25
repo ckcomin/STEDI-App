6769f51bcee281851fe4de5efa6871b8cc262a69
ckcomin <com20009@byui.edu>
2023-05-25 10:32:22 -0600

    assignment

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res)=>{
+
+    res.send("Welcome to my backend API");
+})
+
+app.listen(3000, ()=> {
+
+    console.log("Listening");
+})
